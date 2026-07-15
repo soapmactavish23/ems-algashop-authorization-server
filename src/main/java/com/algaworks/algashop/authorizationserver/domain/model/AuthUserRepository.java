fe53b69e024597088ab3a656f355c459542dc0fc
soapmactavish23 <ricknogueira1231@gmail.com>
2026-07-15 12:19:18 -0300

    7.3. Integrando login com banco de dados

---
+++ src/main/java/com/algaworks/algashop/authorizationserver/domain/model/AuthUserRepository.java
@@ -0,0 +1,12 @@
+package com.algaworks.algashop.authorizationserver.domain.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface AuthUserRepository extends JpaRepository<AuthUser, UUID> {
+
+    Optional<AuthUser> findByEmail(String email);
+
+}
