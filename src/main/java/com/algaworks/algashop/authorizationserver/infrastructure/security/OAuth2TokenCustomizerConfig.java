f8f9e85c2edb114ae70d2932a8ea21a18a8fca9b
soapmactavish23 <ricknogueira1231@gmail.com>
2026-07-16 21:33:11 -0300

    7.5. Customizando Access Token

--- src/main/java/com/algaworks/algashop/authorizationserver/infrastructure/security/OAuth2TokenCustomizerConfig.java
+++ src/main/java/com/algaworks/algashop/authorizationserver/infrastructure/security/OAuth2TokenCustomizerConfig.java
@@ -1,25 +1,58 @@
 package com.algaworks.algashop.authorizationserver.infrastructure.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
 import org.springframework.security.oauth2.core.oidc.endpoint.OidcParameterNames;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 
 @Configuration
+@RequiredArgsConstructor
 public class OAuth2TokenCustomizerConfig {
 
-    @Bean
-    public OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer(OidcUserInfoService oidcUserInfoService) {
-        return context -> {
-            if(OidcParameterNames.ID_TOKEN.equals(context.getTokenType().getValue())) {
-                String email = context.getPrincipal().getName();
+    private final OidcUserInfoService oidcUserInfoService;
 
-                OidcUserInfo oidcUserInfo = oidcUserInfoService.loadUser(email);
+    @Bean
+    public OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer() {
+        return context -> {
+            String tokenType = context.getTokenType().getValue();
+            AuthorizationGrantType authorizationGrantType = context.getAuthorizationGrantType();
+            if(isIdToken(tokenType)) {
+                OidcUserInfo oidcUserInfo = loadUser(context);
                 context.getClaims().claims(claims -> claims.putAll(oidcUserInfo.getClaims()));
+            } else {
+                if(isAcessToken(tokenType) && (isAuthCodeFlow(authorizationGrantType)
+                        || isRefreshTokenFlow(authorizationGrantType))) {
+                    OidcUserInfo oidcUserInfo = loadUser(context);
+                    context.getClaims().subject(oidcUserInfo.getSubject());
+                }
             }
         };
+    }
+
+    private boolean isRefreshTokenFlow(AuthorizationGrantType authorizationGrantType) {
+        return AuthorizationGrantType.REFRESH_TOKEN.equals(authorizationGrantType);
+    }
+
+    private boolean isAuthCodeFlow(AuthorizationGrantType authorizationGrantType) {
+        return AuthorizationGrantType.AUTHORIZATION_CODE.equals(authorizationGrantType);
+    }
+
+    private OidcUserInfo loadUser(JwtEncodingContext context) {
+        String email = context.getPrincipal().getName();
+        return oidcUserInfoService.loadUser(email);
+    }
+
+    private boolean isAcessToken(String tokenType) {
+        return OAuth2TokenType.ACCESS_TOKEN.getValue().equals(tokenType);
+    }
+
+    private static boolean isIdToken(String tokenType) {
+        return OidcParameterNames.ID_TOKEN.equals(tokenType);
     }
 
 }
